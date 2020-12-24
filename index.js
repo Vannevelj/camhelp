e9f72506b0f82e93b0850534e7429e5db497a9de
Jeroen Vannevel <jer_vannevel@outlook.com>
2020-12-24 16:31:47 +0000

    trying to move sentry dsn to github secrets

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-/**
- * @format
- */
-
+import buildinfo from './buildinfo.json';
 import {AppRegistry} from 'react-native';
 import {Navigation} from 'react-native-navigation';
 import {name as appName} from './app.json';
@@ -10,8 +7,7 @@ import Picker from './src/Picker/Picker';
 import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
-  dsn:
-    'https://a866d5f3aa2c42209cd5b872091c3d31@o494690.ingest.sentry.io/5566195',
+  dsn: buildinfo.sentryDsn,
 });
 
 Navigation.registerComponent('Camera', () => Camera);
