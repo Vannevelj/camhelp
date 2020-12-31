bb03d8818018d7a05176582354e0198cbe0b714a
Jeroen Vannevel <jer_vannevel@outlook.com>
2020-12-31 03:06:22 +0000

    Make sure it's not specific to regions

--- src/TextHelper.ts
+++ src/TextHelper.ts
@@ -4,13 +4,13 @@ import * as RNLocalize from 'react-native-localize';
 type TranslationKeys = keyof typeof translations;
 
 export default class TextHelper {
-  public static locale: string = 'en-GB';
+  public static locale: string = 'en';
 
   public static init = () => {
     console.log('initializing locale information');
     const locales = RNLocalize.getLocales();
     console.log(`found these locales: ${JSON.stringify(locales)}`);
-    TextHelper.locale = locales[0]?.languageTag ?? 'en-GB';
+    TextHelper.locale = locales[0]?.languageCode ?? 'en';
     console.log(`Enabling ${TextHelper.locale} as locale`);
   };
 
@@ -22,6 +22,6 @@ export default class TextHelper {
       return preferredLanguage;
     }
 
-    return text['en-GB'];
+    return text['en'];
   };
 }
